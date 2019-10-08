aa7fe4fc43a5f5be2519a34123b330c5f3cc421d
Eric Seidler <eric.seidler@gmail.com>
2019-10-08 13:37:38 -0500

    get basic test to run

---
+++ src/App.spec.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import App from './App';
+
+describe('App', () => {
+    it('snapshot test', () => {
+        renderer.create(
+            <App />,
+        );
+        expect(true).toBe(true);
+    });
+});
